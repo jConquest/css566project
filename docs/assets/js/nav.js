3830bf8f0c131e6a142b5d3a136e3168d06dcf98
Qaif Amaan Shaikh <44881065+qaifmz@users.noreply.github.com>
2019-05-23 22:45:54 -0700

    Update nav.js

--- docs/assets/js/nav.js
+++ docs/assets/js/nav.js
@@ -1,16 +1,7 @@
-// Normal Clicks
-$(function() {
-  $('.toggle-nav').click(function() {
-    $('body').toggleClass('show-nav');
-     return false;
-  });
-  
-});
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-
-// Toggle with hitting of ESC
-$(document).keyup(function(e) {
-	if (e.keyCode == 27) {
-   $('body').toggleClass('show-nav');
-  }
-});
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
